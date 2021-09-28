9c3ef3a0b10718e0b960b1bc08cb43085218251b
Damaris Macías <63810507+damarismc@users.noreply.github.com>
2021-09-28 13:34:13 -0500

    Update usuariosController.java
    controller Usuarios

--- grupo9/src/main/java/com/grupo9/usuariosController.java
+++ grupo9/src/main/java/com/grupo9/usuariosController.java
@@ -1,5 +1,38 @@
 package com.grupo9;
 
-public class usuariosController {
+import java.util.ArrayList;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.grupo9.UsuarioDAO;
+import com.grupo9.UsuarioDTO;
+
+@RestController
+public class UsuarioController {
+	
+	@RequestMapping("/registarUsuario")
+	public void registrarUsuario(UsuarioDTO usuario) {
+		UsuarioDAO Dao = new UsuarioDAO();
+		Dao.registrarUsuario(usuario);
+	}
+	
+	@RequestMapping("/consultarUsuario")
+	public ArrayList<UsuarioDTO> consultarUsuario(int documento){
+		UsuarioDAO Dao = new UsuarioDAO();
+		return Dao.consultarUsuario(documento);
+	}
+	
+	@RequestMapping("/listarUsuarios")
+	public ArrayList<UsuarioDTO> listaDeUsuarios(){
+		UsuarioDAO Dao = new UsuarioDAO();
+		return Dao.listaDeUsuarios();
+	}
+	
+	@RequestMapping("/eliminarUsuario")
+	public void eliminarUsuario(int cedula) {
+		UsuarioDAO Dao = new UsuarioDAO();
+		Dao.eliminarusuario(cedula);
+	}
 }
+
