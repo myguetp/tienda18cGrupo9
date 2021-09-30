113b45af336e10de67c835a7996ef8c05304c189
myguetp <90976818+myguetp@users.noreply.github.com>
2021-09-30 17:18:21 -0500

    Update ClientesDAO.java

--- grupo9/src/main/java/com/grupo9/DAO/ClientesDAO.java
+++ grupo9/src/main/java/com/grupo9/DAO/ClientesDAO.java
@@ -1,14 +1,95 @@
 package com.grupo9.DAO;
 import java.sql.*;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+import com.grupo9.DTO.ClientesDTO;
+
 
 
 public class ClientesDAO {
 	PreparedStatement preparedStatement;
 	
+		public ArrayList <ClientesDTO> listaDeClientes(){
+		ArrayList <ClientesDTO> miCliente=new ArrayList<ClientesDTO>();
+		Conexion conex=new Conexion();
+		
+		try {
+			PreparedStatement consulta=conex.getConnection().prepareStatement("SELECT * FROM clientes");
+			ResultSet res =consulta.executeQuery();
+			while(res.next()) {
+				ClientesDTO clientes=new ClientesDTO();
+				clientes.setCedulaCliente(Integer.parseInt(res.getString("cedula_cliente")));
+				clientes.setDireccionCliente(res.getString("direccion_cliente"));
+				clientes.setEmailCliente(res.getString("email_cliente"));
+				clientes.setNombreCliente(res.getString("nombre_cliente"));
+				clientes.setTelefonoCliente(res.getString("telefono_cliente"));
+				
+				miCliente.add(clientes);
+				}
+				res.close();
+				consulta.close();
+				conex.desconectar();
+		}catch(Exception e) {
+			JOptionPane.showMessageDialog(null, "No se puede realizar la consulta" + e);
+		}
+		return miCliente;
+		}
+	public void registrarCliente(ClientesDTO clientes) {
+		Conexion conex=new Conexion();
+		try {
+			Statement st=conex.getConnection().createStatement();
+			st.executeUpdate("INSERT INTO clientes VALUES('"+clientes.getCedulaCliente()+"', '"+clientes.getDireccionCliente()+"', '"+clientes.getEmailCliente()+"', '"+clientes.getNombreCliente()+"', '"+clientes.getTelefonoCliente()+"')");
+			JOptionPane.showMessageDialog(null, "Se ha registrado exitosamente el cliente", "Información", JOptionPane.INFORMATION_MESSAGE);
+			st.close();
+			conex.desconectar();
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+			JOptionPane.showMessageDialog(null, "No se pudo registrar el cliente");
+		}
+	}
+	public ArrayList <ClientesDTO> consultarClientes(int documento){
+		ArrayList <ClientesDTO> miCliente=new ArrayList <ClientesDTO>();
+		Conexion conex=new Conexion();
+		try {
+			PreparedStatement consulta=conex.getConnection().prepareStatement("SELECT * FROM clientes WHERE cedula_cliente=?");
+			consulta.setInt(1, documento);
+			ResultSet res = consulta.executeQuery();
+			if(res.next()) {
+				ClientesDTO clientes=new ClientesDTO();
+				clientes.setCedulaCliente(Integer.parseInt(res.getString("cedula_cliente")));
+				clientes.setDireccionCliente(res.getString("direccion_cliente"));
+				clientes.setEmailCliente(res.getString("email_cliente"));
+				clientes.setNombreCliente(res.getString("nombre_cliente"));
+				clientes.setTelefonoCliente(res.getString("telefono_cliente"));
+				
+				miCliente.add(clientes);
+			}
+			res.close();
+			consulta.close();
+			conex.desconectar();
+		}catch(Exception e) {
+			JOptionPane.showMessageDialog(null, "No se pudo consultar el cliente" + e);
+		}
+		return miCliente;
+	}
+	public void eliminarCliente(int documento) {
+		Conexion conex=new Conexion();
+		try {
+			String query="DELETE FROM clientes WHERE cedula_cliente=?";
+			preparedStatement=conex.getConnection().prepareStatement(query);
+			preparedStatement.setInt(1, documento);
+			preparedStatement.executeUpdate();
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
 	
 	
 	
 	
+		
 	public void eliminarCliente(int cedula) {
         Conexion conex = new Conexion();
         try {
@@ -21,10 +102,7 @@ public class ClientesDAO {
             System.out.println(e.getMessage());
         }
     }
-	
-	
-	
-	public void actualizarcliente(int cedula) {
+     public void actualizarcliente(int cedula) {
 		Conexion conex = new Conexion();
 		try {
 			String query = "update CLIENTES set  direccion_cliente=?, email_cliente=?, nombre_cliente=?, telefono_cliente=? where cedula_cliente=?";
@@ -32,6 +110,6 @@ public class ClientesDAO {
 		}catch(Exception e) {
 			System.out.println(e.getMessage());
 		}
-	}
+    }
 
 }
