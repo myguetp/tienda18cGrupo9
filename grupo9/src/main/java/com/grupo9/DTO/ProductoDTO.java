7b053479c6e0a3b4f456e1ec79bccb97b5b160b9
Juan Sebasti Sanchez C <47191194+Kaonesis1512@users.noreply.github.com>
2021-10-06 19:31:58 -0500

    Corrección ProductoDTO

--- grupo9/src/main/java/com/grupo9/DTO/ProductoDTO.java
+++ grupo9/src/main/java/com/grupo9/DTO/ProductoDTO.java
@@ -1,105 +1,50 @@
 package com.grupo9.DTO;
 
-import java.util.Objects;
-
 public class ProductoDTO {
-	private int codigoProducto;
-	private double ivaCompra;
-	private int nitProveedor;
+	private Integer codigoProducto;
+	private Double ivaCompra;
+	private Integer nitProveedor;
 	private String nombreProducto;
-	private double precioCompra;
-	private double precioVenta;
-	public ProductoDTO(int codigoProducto, double ivaCompra, int nitProveedor, String nombreProducto,
-			double precioCompra, double precioVenta) {
-
-		this.codigoProducto = codigoProducto;
-		this.ivaCompra = ivaCompra;
-		this.nitProveedor = nitProveedor;
-		this.nombreProducto = nombreProducto;
-		this.precioCompra = precioCompra;
-		this.precioVenta = precioVenta;
-	}
+	private Double precioCompra;
+	private Double precioVenta;
 	
-	public ProductoDTO() {
-	}
-
-	public int getCodigoProducto() {
+	public Integer getCodigoProducto() {
 		return codigoProducto;
 	}
-
-	public void setCodigoProducto(int codigoProducto) {
+	public void setCodigoProducto(Integer codigoProducto) {
 		this.codigoProducto = codigoProducto;
 	}
-
-	public double getIvaCompra() {
+	public Double getIvaCompra() {
 		return ivaCompra;
 	}
-
-	public void setIvaCompra(double ivaCompra) {
+	public void setIvaCompra(Double ivaCompra) {
 		this.ivaCompra = ivaCompra;
 	}
-
-	public int getNitProveedor() {
+	public Integer getNitProveedor() {
 		return nitProveedor;
 	}
-
-	public void setNitProveedor(int nitProveedor) {
+	public void setNitProveedor(Integer nitProveedor) {
 		this.nitProveedor = nitProveedor;
 	}
-
 	public String getNombreProducto() {
 		return nombreProducto;
 	}
-
 	public void setNombreProducto(String nombreProducto) {
 		this.nombreProducto = nombreProducto;
 	}
-
-	public double getPrecioCompra() {
+	public Double getPrecioCompra() {
 		return precioCompra;
 	}
-
-	public void setPrecioCompra(double precioCompra) {
+	public void setPrecioCompra(Double precioCompra) {
 		this.precioCompra = precioCompra;
 	}
-
-	public double getPrecioVenta() {
+	public Double getPrecioVenta() {
 		return precioVenta;
 	}
-
-	public void setPrecioVenta(double precioVenta) {
+	public void setPrecioVenta(Double precioVenta) {
 		this.precioVenta = precioVenta;
 	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(codigoProducto, ivaCompra, nitProveedor, nombreProducto, precioCompra, precioVenta);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ProductoDTO other = (ProductoDTO) obj;
-		return codigoProducto == other.codigoProducto
-				&& Double.doubleToLongBits(ivaCompra) == Double.doubleToLongBits(other.ivaCompra)
-				&& nitProveedor == other.nitProveedor && Objects.equals(nombreProducto, other.nombreProducto)
-				&& Double.doubleToLongBits(precioCompra) == Double.doubleToLongBits(other.precioCompra)
-				&& Double.doubleToLongBits(precioVenta) == Double.doubleToLongBits(other.precioVenta);
-	}
-
-	@Override
-	public String toString() {
-		return "ProductoDTO [codigoProducto=" + codigoProducto + ", ivaCompra=" + ivaCompra + ", nitProveedor="
-				+ nitProveedor + ", nombreProducto=" + nombreProducto + ", precioCompra=" + precioCompra
-				+ ", precioVenta=" + precioVenta + "]";
-	}
 	
-
-
-
+	
+	
 }
