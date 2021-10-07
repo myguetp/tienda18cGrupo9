64daf3d10d73a31ea19196469cf8c69a23f51ba6
Juan Sebasti Sanchez C <47191194+Kaonesis1512@users.noreply.github.com>
2021-10-06 19:36:55 -0500

    Correccion ProductoController

--- grupo9/src/main/java/com/grupo9/ProductoController.java
+++ grupo9/src/main/java/com/grupo9/ProductoController.java
@@ -8,32 +8,28 @@ import org.springframework.web.bind.annotation.RestController;
 import com.grupo9.DAO.ProductoDAO;
 import com.grupo9.DTO.ProductoDTO;
 
-
 @RestController
 public class ProductoController {
-	
+
 	@RequestMapping("/registrarProducto")
-	public void registrarProducto(ProductoDTO producto) {
-		ProductoDAO Dao = new ProductoDAO();
+	public void registrarProducto (ProductoDTO producto) {
+		ProductoDAO Dao=new ProductoDAO();
 		Dao.registrarProducto(producto);
 	}
-	
 	@RequestMapping("/consultarProducto")
-	public ArrayList<ProductoDTO> consultarProducto(int codProducto){
-		ProductoDAO Dao = new ProductoDAO();
-		return Dao.consultarProducto(codProducto);
+	public ArrayList <ProductoDTO> consultarProducto (int codigo){
+		ProductoDAO Dao=new ProductoDAO();
+		return Dao.consultarProducto(codigo);
 	}
-	
-	@RequestMapping("/listarProducto")
-	public ArrayList<ProductoDTO> listaDeProducto(){
-		ProductoDAO Dao = new ProductoDAO();
+	@RequestMapping("/listaDeProducto")
+	public ArrayList <ProductoDTO> listaDeProducto(){
+		ProductoDAO Dao=new ProductoDAO();
 		return Dao.listaDeProducto();
 	}
-	
 	@RequestMapping("/eliminarProducto")
-	public void eliminarProducto(int codProducto) {
-		ProductoDAO Dao = new ProductoDAO();
-		Dao.eliminarProducto(codProducto);
+	public void eliminarProducto(int codigo) {
+		ProductoDAO Dao=new ProductoDAO();
+		Dao.eliminarProducto(codigo);
 	}
 	
 	@RequestMapping("/editarProducto")
@@ -42,5 +38,3 @@ public class ProductoController {
 		Dao.editarProducto(producto);
 	}
 
-
-}
